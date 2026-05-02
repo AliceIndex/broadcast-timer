f925657fa4437806d0150960eddaaaa929385045
Alice Index <75057710+AliceIndex@users.noreply.github.com>
2026-05-03 02:49:24 +0900

    エラーの修正

--- infra/broadcast_timer_infra.go
+++ infra/broadcast_timer_infra.go
@@ -21,11 +21,9 @@ type TimerStackProps struct {
 func NewBroadcastTimerStack(scope constructs.Construct, id string, props *TimerStackProps) awscdk.Stack {
 	stack := awscdk.NewStack(scope, &id, &props.StackProps)
 
-	// リソース名に環境名を付与
 	funcName := fmt.Sprintf("WebSocketHandler-%s", props.EnvName)
 	apiName := fmt.Sprintf("BroadcastTimerAPI-%s", props.EnvName)
 
-	// Lambda関数の定義
 	handler := awslambda.NewFunction(stack, jsii.String("WebSocketHandler"), &awslambda.FunctionProps{
 		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
 		FunctionName: jsii.String(funcName),
@@ -33,40 +31,28 @@ func NewBroadcastTimerStack(scope constructs.Construct, id string, props *TimerS
 		Code:         awslambda.Code_FromAsset(jsii.String("../backend/main.zip"), nil),
 	})
 
-	// IAM権限の付与
 	handler.AddToRolePolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
 		Actions:   jsii.Strings("execute-api:ManageConnections"),
 		Resources: jsii.Strings("arn:aws:execute-api:*:*:*/*/*/*"),
 	}))
 
-	// WebSocket APIの作成
 	webSocketApi := awsapigatewayv2.NewWebSocketApi(stack, jsii.String("BroadcastTimerAPI"), &awsapigatewayv2.WebSocketApiProps{
 		ApiName: jsii.String(apiName),
 	})
 
-	// Lambda統合の設定
 	integration := awsapigatewayv2integrations.NewWebSocketLambdaIntegration(jsii.String("HandlerIntegration"), handler, nil)
 
-	// ルートの設定
 	webSocketApi.AddRoute(jsii.String("$connect"), &awsapigatewayv2.WebSocketRouteOptions{Integration: integration})
 	webSocketApi.AddRoute(jsii.String("$disconnect"), &awsapigatewayv2.WebSocketRouteOptions{Integration: integration})
 	webSocketApi.AddRoute(jsii.String("$default"), &awsapigatewayv2.WebSocketRouteOptions{Integration: integration})
 
-	// ステージの作成
 	awsapigatewayv2.NewWebSocketStage(stack, jsii.String("Stage"), &awsapigatewayv2.WebSocketStageProps{
 		WebSocketApi: webSocketApi,
 		StageName:    jsii.String(props.EnvName),
 		AutoDeploy:   jsii.Bool(true),
 	})
 
-	// URLを出力
-	apiEndpoint := fmt.Sprintf("wss://%s.execute-api.%s.amazonaws.com/%s", *webSocketApi.ApiId(), *stack.Region(), props.EnvName)
-	awscdk.NewCfnOutput(stack, jsii.String("WebSocketURL"), &awscdk.CfnOutputProps{
-		Value: jsii.String(apiEndpoint),
-	})
-
-	// 【修正1】returnの前にDynamoDBの処理を移動しました
-	// DynamoDBテーブルの作成
+	// 1. 既存の ConnectionsTable (名簿用)
 	connectionsTable := awsdynamodb.NewTable(stack, jsii.String("ConnectionsTable"), &awsdynamodb.TableProps{
 		PartitionKey: &awsdynamodb.Attribute{
 			Name: jsii.String("connectionId"),
@@ -75,26 +61,28 @@ func NewBroadcastTimerStack(scope constructs.Construct, id string, props *TimerS
 		BillingMode:   awsdynamodb.BillingMode_PAY_PER_REQUEST,
 		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
 	})
+	connectionsTable.GrantFullAccess(handler)[cite: 2]
+	handler.AddEnvironment(jsii.String("CONNECTIONS_TABLE"), connectionsTable.TableName(), nil)[cite: 2]
 
-	// 【修正2】権限と環境変数を渡す対象を "handler" に修正しました
-	connectionsTable.GrantFullAccess(handler)
-	handler.AddEnvironment(jsii.String("CONNECTIONS_TABLE"), connectionsTable.TableName(), nil)
-
-	// --- 追加：ルーム状態管理用のDynamoDBテーブル ---
+	// 2. 新設 RoomStatesTable (部屋の状態用)[cite: 1, 2]
 	roomStatesTable := awsdynamodb.NewTable(stack, jsii.String("RoomStatesTable"), &awsdynamodb.TableProps{
 		PartitionKey: &awsdynamodb.Attribute{
-			Name: jsii.String("room_id"), // 部屋番号をキーにする
+			Name: jsii.String("room_id"),
 			Type: awsdynamodb.AttributeType_STRING,
 		},
-		BillingMode:   awsdynamodb.BillingMode_PAY_PER_REQUEST,[cite: 2]
-		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,[cite: 2]
+		BillingMode:   awsdynamodb.BillingMode_PAY_PER_REQUEST,
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
+	})
+	roomStatesTable.GrantFullAccess(handler)[cite: 2]
+	handler.AddEnvironment(jsii.String("ROOM_STATES_TABLE"), roomStatesTable.TableName(), nil)[cite: 2]
+
+	// URLを出力[cite: 2]
+	apiEndpoint := fmt.Sprintf("wss://%s.execute-api.%s.amazonaws.com/%s", *webSocketApi.ApiId(), *stack.Region(), props.EnvName)
+	awscdk.NewCfnOutput(stack, jsii.String("WebSocketURL"), &awscdk.CfnOutputProps{
+		Value: jsii.String(apiEndpoint),
 	})
 
-	// Lambda (handler) に新しいテーブルの権限と環境変数を付与
-	roomStatesTable.GrantFullAccess(handler)
-	handler.AddEnvironment(jsii.String("ROOM_STATES_TABLE"), roomStatesTable.TableName(), nil)
-
-	// スタックの構築がすべて終わってから返す
+	// ★必ず最後！関数の戻り値[cite: 2]
 	return stack
 }
 
